a982367de1d752af6f5ccc6554a994c5c7b07df0
DanielaMadrigal <danielasofiamm@hotmail.com>
2022-04-21 12:59:38 -0600

    objectclass

---
+++ Object.h
@@ -0,0 +1,15 @@
+//
+// Created by Daniela Madrigal on 21/4/2022.
+//
+
+#ifndef PROYECTO_1_EIF204_OBJECT_H
+#define PROYECTO_1_EIF204_OBJECT_H
+#include <iostream>
+
+class Object {
+public:
+    virtual std::string toString() = 0;
+};
+
+
+#endif //PROYECTO_1_EIF204_OBJECT_H
